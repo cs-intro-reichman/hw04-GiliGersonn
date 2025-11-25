385500f0eeca4958accc7b17170f177c1254d8fb
Gili Gerson <gili.gerson@post.runi.ac.il>
2025-11-25 08:03:39 +0200

    test4

--- KeywordsDetector.java
+++ KeywordsDetector.java
@@ -21,6 +21,19 @@ public class KeywordsDetector {
     // Iterates through all the sentences.
     // If a sentence contains one or more of the kewords, prints it.
     public static void detectAndPrint(String[] sentences, String[] keywords) {
-        // Replace this comment with your code
+        for (int i = 0; i < sentences.length; i++) {
+            String sentence = sentences[i];
+            String sentenceLower = Mystring.lowercase(sentence);
+            boolean found = false;
+            for ( int j = 0; j <keywords.length; j++) {
+                string KeywordLower = Mystring.lowercase(keywords[j]);
+            }
+        }
+        
+        for (int i = 0; i < ; i++) {
+
+            
+        }
     }
 }
+
